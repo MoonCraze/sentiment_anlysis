d2d909529a9b0387469a201891ee17742cfe239d
visal <amdbadikari@gmil.com>
2025-06-25 17:25:24 +0530

    architect change

--- model_loader/berta_models.py
+++ model_loader/berta_models.py
@@ -1,4 +1,5 @@
-from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification, AutoModelForSequenceClassification
+
 
 def load_deberta_ner_model():
     model_name = "kamalkraj/deberta-v3-base-ner"
@@ -6,3 +7,10 @@ def load_deberta_ner_model():
     model = AutoModelForTokenClassification.from_pretrained(model_name)
     ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
     return ner_pipeline
+
+def load_deberta_sentiment_model():
+    model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    return sentiment_pipeline
