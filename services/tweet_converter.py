fe7fbe1f0a31e11cacd7ac307ee6d22c2421e28f
visal <amdbadikari@gmil.com>
2025-06-28 23:16:38 +0530

    new update be done

--- services/tweet_converter.py
+++ services/tweet_converter.py
@@ -4,7 +4,7 @@ from preprocessing.preprocess import preprocess_data
 
 
 def run_preprocessing_news():
-    input_path = os.path.join("..", "data", "x_data.json")
-    output_path = os.path.join("..", "data", "preprocessed_data_news.json")
+    input_path = os.path.join("..", "test data", "x_data.json")
+    output_path = os.path.join("..", "test data", "preprocessed_data_news.json")
     preprocess_data(input_path, output_path)
     return output_path
